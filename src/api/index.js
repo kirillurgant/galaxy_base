52174aad648086080d8802f544b64e9598dff439
Kirill Kvashonin <k.kvashonin@tradesoft.ru>
2019-08-28 17:58:46 +0300

    init api

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,19 @@
-// TODO:
+import axios from 'axios';
+
+class Api {
+  constructor(baseURL) {
+    this.axiosInstance = axios.create({
+      baseURL,
+    });
+  }
+
+  getStarships() {
+    return this.axiosInstance.get('/starships');
+  }
+
+  getStarshipById(id) {
+    return this.axiosInstance.get(`/starships/${id}`);
+  }
+}
+
+export default new Api('https://swapi.co/api');
